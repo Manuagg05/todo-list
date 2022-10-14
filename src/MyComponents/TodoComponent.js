1b2ec7038f43657bca43fd0246474c58173bdfa3
manu aggarwal <manuaggarwal2001@gmail.com>
2022-10-14 12:53:41 +0530

    changed

---
+++ src/MyComponents/TodoComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { AddTodo } from "./AddTodo";
+import { Todos } from "./Todos";
+
+const TodoComponent = () => {
+  return (
+      <>
+      <AddTodo/>
+      <Todos/>
+      </>
+  );
+};
+
+export default TodoComponent;
